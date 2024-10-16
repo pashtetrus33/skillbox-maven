ec5b3d2dfbc61d93bee25073220290f9d271de98
Pavel Bakanov <pashtetrus@gmail.com>
2024-10-16 19:26:43 -0300

    module 3 hw

---
+++ src/main/java/ru/skillbox/App.java
@@ -0,0 +1,21 @@
+package ru.skillbox;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    /**
+     * Приватный конструктор - требования checkstyle плагина (методы - стат).
+     */
+    private App() {
+    }
+
+    /**
+     * Главный метод приложения.
+     *
+     * @param args параметры командной строки
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");
+    }
+}
